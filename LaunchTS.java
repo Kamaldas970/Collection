767219907d180d896300cc60c4bf7f66b0a1b834
Kamaldas970 <baleraokamal97050sravan@gmail.com>
2026-03-21 20:13:25 +0530

    Refactor TreeSet usage in LaunchTs class
    Updated the TreeSet operations to include ceiling, floor, and headSet methods.

--- LaunchTS.java
+++ LaunchTS.java
@@ -5,13 +5,15 @@ public class LaunchTs{
 
         ts.add(20);
         ts.add(30);
-        ts.add(100);
         ts.add(80);
+        ts.add(100);
         ts.add(1000);
-//Duplicates are not allowed
-//null value gives nullpointerException
-          System.out.println("The Higher is "+ts.higher(50)); //Single higher element // (80)
-        System.out.println("The lower is "+ts.lower(50)); //Single Lower element // (80)
+        System.out.println("The Higher is "+ts.higher(60));//Single Higher element // (80)
+        System.out.println("The lower is "+ts.lower(50)); //Single lower element // (20)
+        System.out.println(ts.ceiling(65));
+        System.out.println(ts.floor(55));
+            System.out.println(ts.headSet(100));
+
         System.out.println(ts);
     }
     }

8ffc0cfe29ccc159f59463507a26a7a503299dd5
Kamaldas970 <baleraokamal97050sravan@gmail.com>
2026-03-21 19:28:12 +0530

    Add files via upload

---
+++ LaunchTS.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+public class LaunchTs{
+    public static void main(String args[]){
+        TreeSet<Integer> ts=new TreeSet<>();
+
+        ts.add(20);
+        ts.add(30);
+        ts.add(100);
+        ts.add(80);
+        ts.add(1000);
+
+        System.out.println(ts);
+    }
+    }
